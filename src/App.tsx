d8fa28bf83aa35a4517a8d3e6463a13465caf532
Deepak <deepak.oli@nimble-cr.com>
2025-02-23 00:27:34 +0545

    feat: ui improvement

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,22 @@ function App() {
           onClick={disconnectWallet}
           className="wallet-button wallet-button--disconnect"
         >
-          Disconnect Wallet
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="icon"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+
+          <span>Disconnect Wallet</span>
         </button>
 
         {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
--- src/components/Home/WalletConnect.tsx
+++ src/components/Home/WalletConnect.tsx
@@ -16,7 +16,21 @@ const ConnectWallet: React.FC<ConnectWalletProps> = ({
       onClick={connectWallet}
       disabled={isLoading}
     >
-      Connect Wallet
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"
+        />
+      </svg>
+      <span>Connect Wallet</span>
     </button>
     <p>
       Install the{" "}
